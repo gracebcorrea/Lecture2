dc0e10be7b50fcf1f8d46f5dea023dcdd18ba046
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-05-27 15:53:27 -0300

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,6 @@
 ages = {"Alice":22,"Bob":27}
-ages["Charlie"]=30
-ages["Alice"]+=1
+ages["Charlie"] = 30
+ages["Alice"] += 1  #incrementa a idade em um
+ages["Bob"] = ages["Bob"] + 3 #outra maneira de incrementar a idade
 
 print (ages)
