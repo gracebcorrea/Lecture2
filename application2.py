3e78d3a7d1ed15c974a6ae98ea0c7ca55ae2a865
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-05-27 20:14:25 -0300

    Create application2.py

---
+++ application2.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+    def index():
+        return render_template("index.html")
+
+#$ export FLASK_APP=application2.py
+#$ export FLASK_ENV=development
+#flask run --host=0.0.0.0
