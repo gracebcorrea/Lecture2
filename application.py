1cbf00f9518631ec352e61a5d6e367317c47fca9
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-05-27 19:54:15 -0300

    Update application.py

--- application.py
+++ application.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+@app.route("/Grace")
+    def Grace():
+        return "Hello World! I am Here :)"
 
 #$ export FLASK_APP=application.py
 #$ export FLASK_ENV=development
+#flask run --host=0.0.0.0
