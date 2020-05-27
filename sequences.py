bfa264f3ceefc6c211509db7ad699889b1a40f50
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-05-27 14:54:30 -0300

    Update sequences.py

--- sequences.py
+++ sequences.py
@@ -1,3 +1,14 @@
+#List - array
+
 name = "Alice"
 coordinates = (10.0,20.0)
-names ["Alice","Bob","Charlie"]
+names = ["Alice","Bob","Charlie"]
+
+#First Caracter of the name
+name = "Alice"
+name[0]
+
+#primeiro nome da lista
+names[0]
+names[1]
+names[2]
