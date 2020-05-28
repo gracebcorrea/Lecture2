fbf82b6782cc5d2d928261f894773bd4fd8c1b02
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-05-28 20:11:34 -0300

    Delete app.py

--- Flask/app.py
+++
@@ -1,9 +0,0 @@
-#eu quero criar uma nova aplicação e quero que seja uma app web usando Flask
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello World!"
