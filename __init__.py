3276e5134c587189e2db323b790522f910f04f7c
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-05-28 23:50:17 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,9 @@
-#eu quero criar uma nova aplicação e quero que seja uma app web usando Flask
-
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
-
-@app.route("/Grace")
-    def Grace():
-        return "Hello World! I am Here :)"
-
-@app.route("/<string:name>")
-    def name():
-        return f "Hello, {name}! Good to see you)"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 #$ export FLASK_APP=__init__.py
 #$ export FLASK_ENV=development
